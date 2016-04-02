b5ff1582838bda8b60f54200c3c52f3e55278100
Keith Stellyes <keithastellyes@gmail.com>
2016-04-02 13:04:06 -0700

    inital commit - testing

---
+++ Assets/Scripts/PlayMovieOnSpace.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovieOnSpace : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            Renderer r = GetComponent<Renderer>();
+            AudioSource audio = GetComponent<AudioSource>();
+            MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+            if (movie.isPlaying)
+            {
+                movie.Pause();
+                audio.Pause();
+            }
+            else {
+                movie.Play();
+                audio.Play();
+            }
+        }
+    }
+}
